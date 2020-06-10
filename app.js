7eac9111d80fec350bacc0c8a5005a5f15691521
r.collas <robin.collas@hotmail.fr>
2020-06-10 15:11:52 +0200

    config eslint & prettier

--- app.js
+++ app.js
@@ -9,26 +9,31 @@ const app = express();
 
 //Set up default mongoose connection
 var mongoDB = 'mongodb://' + config.mongodb.host + ':' + config.mongodb.port + '/' + config.mongodb.db;
-mongoose.connect(mongoDB, { useNewUrlParser: true })
-.then(() => {
-  console.log('Connection to database has been established successfully.');
-})
-.catch(err => {
-  console.log('Unable to connect to the database:', err);
-});
+mongoose
+	.connect(mongoDB, { useNewUrlParser: true })
+	.then(() => {
+		console.log('Connection to database has been established successfully.');
+	})
+	.catch((err) => {
+		console.log('Unable to connect to the database:', err);
+	});
 
-app.use(cors({origin:true,credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  console.log("received : "+req.method+" "+req.originalUrl+" (Authorization: "+req.get("Authorization")+")");
-  next();
+	console.log(
+		'received : ' + req.method + ' ' + req.originalUrl + ' (Authorization: ' + req.get('Authorization') + ')'
+	);
+	next();
 });
 app.use('/', routes);
 
 app.listen(config.app.port, function () {
-  console.log('[ENV='+config.app.env+'] Application running on port '+config.app.port);
-})
+	console.log('[ENV=' + config.app.env + '] Application running on port ' + config.app.port);
+});
