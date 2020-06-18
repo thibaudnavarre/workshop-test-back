acddfcc0a9fb30dc0ac98ca75d3a5baa90525495
r.collas <robin.collas@hotmail.fr>
2020-06-18 17:25:42 +0200

    add config.js and app.js to linter

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const routes = require('./app/routes');
 const bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var cors = require('cors');
-var config = require('./config');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./app/routes');
+const config = require('./config');
 
 const app = express();
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://' + config.mongodb.host + ':' + config.mongodb.port + '/' + config.mongodb.db;
+// Set up default mongoose connection
+const mongoDB = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
 mongoose
 	.connect(mongoDB, { useNewUrlParser: true })
 	.then(() => {
+		// eslint-disable-next-line no-console
 		console.log('Connection to database has been established successfully.');
 	})
 	.catch((err) => {
+		// eslint-disable-next-line no-console
 		console.log('Unable to connect to the database:', err);
 	});
 
@@ -27,11 +29,13 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-	console.log('received : ' + req.method + ' ' + req.originalUrl);
+	// eslint-disable-next-line no-console
+	console.log(`received : ${req.method} ${req.originalUrl}`);
 	next();
 });
 app.use('/', routes);
 
 app.listen(config.app.port, function () {
-	console.log('[ENV=' + config.app.env + '] Application running on port ' + config.app.port);
+	// eslint-disable-next-line no-console
+	console.log(`[ENV=${config.app.env}] Application running on port ${config.app.port}`);
 });
