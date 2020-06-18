221a3b168db679c789a47101a38dbd864696c735
Thibaud Navarre <navarre.thibaud@gmail.com>
2020-06-18 16:52:18 +0200

    Fix english wording

--- app/services/game-orchestrator/game-orchestrator.service.spec.js
+++ app/services/game-orchestrator/game-orchestrator.service.spec.js
@@ -109,7 +109,7 @@ describe('game-orchestrator.service', () => {
 			});
 		});
 
-		describe('moles positionning', () => {
+		describe('moles positioning', () => {
 			beforeEach(() => {
 				jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
 			});
